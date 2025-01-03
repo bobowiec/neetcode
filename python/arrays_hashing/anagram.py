d48d1804d45e8712ac10cd9834f351ff814b65b4
Lukasz Bobowiec <lukasz.bobowiec@sabre.com>
2025-01-03 22:34:28 +0100

    updates

--- python/arrays_hashing/anagram.py
+++ python/arrays_hashing/anagram.py
@@ -20,6 +20,26 @@ class Solution:
         
         return sorted(s) == sorted(t)
 
+    # hash table: time complexity O(n), space complexity O(N)
+    def isAnagramHash(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_count = {}
+        t_count = {}
+        for i in range(len(s)):
+            if s[i] in s_count:
+                s_count[s[i]] += 1
+            else:
+                s_count[s[i]] = 1
+            
+            if t[i] in t_count:
+                t_count[t[i]] += 1
+            else:
+                t_count[t[i]] = 1
+        
+        return s_count == t
+
     # hash table: time complexity O(n), space complexity O(1)
     def isAnagramOpt(self, s: str, t: str) -> bool:
         if len(s) != len(t):
--- python/arrays_hashing/two_sum.py
+++ python/arrays_hashing/two_sum.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
 
-    # brute force: time complexity O(n^2), space complexity O(1)
+    # brute force: time complexity O(N^2), space complexity O(1)
     def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
@@ -11,7 +11,7 @@ class Solution:
                     return [i, j]
         return []
     
-    # two pointers technique: time complexity O(nlogn), space complexity O(1)
+    # two pointers technique: time complexity O(N*logN), space complexity O(N)
     def twoSumTwoPointers(self, nums: List[int], target: int) -> List[int]:
         nums = [(num, i) for i, num in enumerate(nums)]
         nums.sort()
@@ -25,7 +25,7 @@ class Solution:
                 right -= 1
         return []
     
-    # two pass hash maps: time complexity O(n), space complexity O(n)
+    # two passes over hash maps: time complexity O(N), space complexity O(N)
     def twoSumTwoPassHashMaps(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
         for i, num in enumerate(nums):
@@ -37,7 +37,7 @@ class Solution:
                 return [i, hash_map[diff]]
         return []
 
-    # hash table: time complexity O(n), space complexity O(n)
+    # one pass over hash table: time complexity O(N), space complexity O(N)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
         for i, num in enumerate(nums):
